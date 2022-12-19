a8ef5e58e74226548931ff88175c2e194edb2297
Ulyana <howerlor@mail.ru>
2022-12-19 22:41:24 +0400

    Выбор роли

---
+++ FormsHjemme/FormsHjemme/Form1.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FormsHjemme
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Form3 users = new Form3();
+            users.Show();
+            this.Hide();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Form2 users = new Form2();
+            users.Show(); 
+            this.Hide();
+        }
+    }
+}
